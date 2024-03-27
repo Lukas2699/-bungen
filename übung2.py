d6c123da42b202880a7d9ce9aadbe540e15f7d11
Lukas2699 <162427416+Lukas2699@users.noreply.github.com>
2024-03-27 11:07:28 +0100

    s

---
+++ übung2.py
@@ -0,0 +1,48 @@
+import math
+
+class Vector3:
+    def __init__(self, x, y, z):
+        self.x = x
+        self.y = y
+        self.z = z
+    
+    def __str__(self):
+        return f"({self.x}, {self.y}, {self.z})"
+    
+    def __add__(self, other):
+        return Vector3(self.x + other.x, self.y + other.y, self.z + other.z)
+    
+    def __sub__(self, other):
+        return Vector3(self.x - other.x, self.y - other.y, self.z - other.z)
+    
+    def __mul__(self, other):
+        if isinstance(other, Vector3):
+            return Vector3(self.x * other.x, self.y * other.y, self.z * other.z)
+        elif isinstance(other, (int, float)):
+            return Vector3(self.x * other, self.y * other, self.z * other)
+    
+    def __rmul__(self, other):
+        return self.__mul__(other)
+    
+    def dot(self, other):
+        return self.x * other.x + self.y * other.y + self.z * other.z
+    
+    def cross(self, other):
+        return Vector3(self.y * other.z - self.z * other.y,
+                       self.z * other.x - self.x * other.z,
+                       self.x * other.y - self.y * other.x)
+    
+    def normalize(self):
+        magnitude = math.sqrt(self.x ** 2 + self.y ** 2 + self.z ** 2)
+        if magnitude == 0:
+            return Vector3(0, 0, 0)
+        else:
+            return Vector3(self.x / magnitude, self.y / magnitude, self.z / magnitude)
+
+#Vektoren
+a=Vector3(3,4,2)
+b=Vector3(2,1,0)
+
+v1=a+b
+print(v1)
+
